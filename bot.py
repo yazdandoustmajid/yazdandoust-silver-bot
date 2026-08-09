d556cb01a9d4513718f56c88890562a1654a3e98
yazdandoustmajid <yazdandoust.majid@gmail.com>
2026-08-10 01:36:03 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -685,16 +685,17 @@ draw_center(
     )
 
 
-output = BytesIO()
+    output = BytesIO()
 
-img.save(
+        img.save(
         output,
         "PNG"
- )
+  
+   )
 
-output.seek(0)
+    output.seek(0)
 
-return output
+    return output
 
 
 # =========================
